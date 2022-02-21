f1964c626aa9bda2efc7a3471225bf94e1507439
jwang0110 <johnw7@uci.edu>
2022-02-20 16:48:49 -0800

    handle failed promises

--- controllers/database.js
+++ controllers/database.js
@@ -2,9 +2,12 @@ const MongoClient = require("mongodb").MongoClient;
 
 const handleDatabase = async (req, res) => {
 	const client = new MongoClient(process.env.MONGODB_URI, {
+		useNewUrlParser: true,
 		useUnifiedTopology: true,
 	});
-	client.connect(async (err) => {
+
+	try {
+		await client.connect();
 		const collection = client.db("vocab").collection("data");
 		// perform actions on the collection object
 		const cursor = await collection.find();
@@ -14,8 +17,11 @@ const handleDatabase = async (req, res) => {
 		} else {
 			res.status(400).json("failed");
 		}
+	} catch (e) {
+		res.status(400).json(e);
+	} finally {
 		client.close();
-	});
+	}
 };
 
 module.exports = {
